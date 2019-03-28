d785010c323f83e6073a5e2f37f8cf073d856337
seahrh <seahrh@gmail.com>
2019-03-28 19:43:57 +0800

    fix spark/hive/glue config

--- aws/create-cluster.sh
+++ aws/create-cluster.sh
@@ -2,8 +2,8 @@
 
 aws emr --profile seahrh create-cluster --name "spark_cluster" \
 --release-label emr-5.22.0 \
---applications Name=Spark Name=Hive Name=Hue Name=Tez Name=HCatalog \
+--applications Name=Spark Name=Hive Name=Hue Name=Tez Name=HCatalog Name=Ganglia \
 --configurations https://s3-ap-southeast-1.amazonaws.com/com.sgcharts.ap-southeast-1/emr/configurations.json \
 --ec2-attributes KeyName=emr \
---instance-type m4.large --instance-count 1 \
+--instance-type r5.xlarge --instance-count 1 \
 --use-default-roles
---
+++ spark/clean.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+spark-submit --master yarn \
+--deploy-mode cluster \
+--class com.sgcharts.badrenter.Cleaner \
+s3://com.sgcharts.ap-southeast-1/deploy/bad-renter-assembly-0.1.0.jar \
+\
+--src_db rent \
+--src_table raw \
+--sink_db rent \
+--sink_table payment \
+--sink_partition ds='20190326' \
+--sink_path s3://com.sgcharts.ap-southeast-1/hive/warehouse/rent.db/payment/ds=20190326
