776a034d3619ccc40677bf8e4cac4f409a4be57c
seahrh <seahrh@gmail.com>
2019-03-29 15:53:29 +0800

    fix newline separator

--- spark/clean.sh
+++ spark/clean.sh
@@ -10,5 +10,5 @@ s3://com.sgcharts.ap-southeast-1/deploy/bad-renter-assembly-0.1.0.jar \
 --sink_db rent \
 --sink_table payment \
 --sink_partition ds='20190326' \
---sink_path s3://com.sgcharts.ap-southeast-1/hive/warehouse/rent.db/payment/ds=20190326
---default_dob 19800101 \
+--sink_path s3://com.sgcharts.ap-southeast-1/hive/warehouse/rent.db/payment/ds=20190326 \
+--default_dob 19800101
