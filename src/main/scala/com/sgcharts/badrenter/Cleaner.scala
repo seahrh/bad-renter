c96a85c6dc033ab0a335c6749c72a69b64ed5625
seahrh <seahrh@gmail.com>
2019-03-31 17:06:46 +0800

    refactor move case class as inner class of Cleaner

--- src/main/scala/com/sgcharts/badrenter/Cleaner.scala
+++ src/main/scala/com/sgcharts/badrenter/Cleaner.scala
@@ -1,11 +1,10 @@
 package com.sgcharts.badrenter
 
+import java.sql.Date
 import java.time.format.DateTimeFormatter
 import java.time.{LocalDate, Period, ZoneOffset}
-import java.sql.Date
 
-import com.sgcharts.badrenter.Cleaner.Params
-import org.apache.spark.sql.{DataFrame, Dataset, Encoders, Row, SparkSession}
+import org.apache.spark.sql._
 
 object Cleaner extends Log4jLogging {
   private val APP_NAME: String = getClass.getName
@@ -100,109 +99,111 @@ object Cleaner extends Log4jLogging {
     }
   }
 
-  private[badrenter] final case class Params(
-                                              srcDb: String = "",
-                                              srcTable: String = "",
-                                              sinkDb: String = "",
-                                              sinkTable: String = "",
-                                              sinkPartition: String = "",
-                                              sinkPath: String = "",
-                                              defaultDob: String = ""
-                                            )
+  private final case class Params(
+                                   srcDb: String = "",
+                                   srcTable: String = "",
+                                   sinkDb: String = "",
+                                   sinkTable: String = "",
+                                   sinkPartition: String = "",
+                                   sinkPath: String = "",
+                                   defaultDob: String = ""
+                                 )
 
-}
+  // Schema of the sink table
+  @SerialVersionUID(1L)
+  private final case class Payment(
+                                    id: Long,
+                                    name: String,
+                                    age: Int,
+                                    house_id: Int,
+                                    house_zip: Int,
+                                    payment_date: Date,
+                                    payment_date_year: Int,
+                                    payment_date_month: Int,
+                                    payment_date_day_of_week: Int,
+                                    payment_date_day_of_month: Int,
+                                    payment_amount: Int,
+                                    rent_amount: Int,
+                                    default_amount: Int
+                                  )
 
-// Schema of the sink table
-@SerialVersionUID(1L)
-private final case class Payment(
-                                  id: Long,
-                                  name: String,
-                                  age: Int,
-                                  house_id: Int,
-                                  house_zip: Int,
-                                  payment_date: Date,
-                                  payment_date_year: Int,
-                                  payment_date_month: Int,
-                                  payment_date_day_of_week: Int,
-                                  payment_date_day_of_month: Int,
-                                  payment_amount: Int,
-                                  rent_amount: Int,
-                                  default_amount: Int
-                                )
+  private object Payment {
+    private val today: LocalDate = LocalDate.now(ZoneOffset.UTC)
 
-private object Payment {
-  private val today: LocalDate = LocalDate.now(ZoneOffset.UTC)
-
-  private[badrenter] def from(row: Row, params: Params): Payment = {
-    val name = row.getAs[String]("name")
-    val dob_str = row.getAs[String]("dob")
-    val house_id = row.getAs[Int]("house_id")
-    val house_zip = row.getAs[Int]("house_zip")
-    val payment_date_str = row.getAs[String]("payment_date")
-    val opa: Option[Int] = Option(row.getAs[Int]("payment_amount"))
-    val rent_amount = row.getAs[Int]("rent_amount")
-    val id = row.getAs[Long]("id")
-    val dob: LocalDate = cleanDob(dobStr = dob_str, defaultDobStr = params.defaultDob)
-    val age: Int = Period.between(dob, today).getYears
-    val payment_date_ld: LocalDate = toLocalDate(payment_date_str)
-    val payment_date_year: Int = payment_date_ld.getYear
-    val payment_date_month: Int = payment_date_ld.getMonthValue
-    val payment_date_day_of_week: Int = payment_date_ld.getDayOfWeek.getValue
-    val payment_date_day_of_month: Int = payment_date_ld.getDayOfMonth
-    val payment_date: Date = toDate(payment_date_ld)
-    val payment_amount: Int = paymentAmount(opa, rent_amount)
-    val default_amount: Int = rent_amount - payment_amount
-    Payment(
-      id = id,
-      name = name,
-      age = age,
-      house_id = house_id,
-      house_zip = house_zip,
-      payment_date = payment_date,
-      payment_date_year = payment_date_year,
-      payment_date_month = payment_date_month,
-      payment_date_day_of_week = payment_date_day_of_week,
-      payment_date_day_of_month = payment_date_day_of_month,
-      payment_amount = payment_amount,
-      rent_amount = rent_amount,
-      default_amount = default_amount
-    )
-  }
-
-  private def paymentAmount(opa: Option[Int], ra: Int): Int = opa match {
-    case Some(pa) =>
-      Math.min(Math.abs(pa), ra)
-    case _ => 0
-  }
-
-  private def toDate(localDate: LocalDate): Date = {
-    Date.valueOf(localDate)
-  }
-
-  private def toLocalDate(dateStr: String): LocalDate = {
-    val pattern: String = if (dateStr contains "/") {
-      "MM/dd/yyyy"
-    } else {
-      "yyyyMMdd"
+    private[badrenter] def from(row: Row, params: Params): Payment = {
+      val name = row.getAs[String]("name")
+      val dob_str = row.getAs[String]("dob")
+      val house_id = row.getAs[Int]("house_id")
+      val house_zip = row.getAs[Int]("house_zip")
+      val payment_date_str = row.getAs[String]("payment_date")
+      val opa: Option[Int] = Option(row.getAs[Int]("payment_amount"))
+      val rent_amount = row.getAs[Int]("rent_amount")
+      val id = row.getAs[Long]("id")
+      val dob: LocalDate = cleanDob(dobStr = dob_str, defaultDobStr = params.defaultDob)
+      val age: Int = Period.between(dob, today).getYears
+      val payment_date_ld: LocalDate = toLocalDate(payment_date_str)
+      val payment_date_year: Int = payment_date_ld.getYear
+      val payment_date_month: Int = payment_date_ld.getMonthValue
+      val payment_date_day_of_week: Int = payment_date_ld.getDayOfWeek.getValue
+      val payment_date_day_of_month: Int = payment_date_ld.getDayOfMonth
+      val payment_date: Date = toDate(payment_date_ld)
+      val payment_amount: Int = paymentAmount(opa, rent_amount)
+      val default_amount: Int = rent_amount - payment_amount
+      Payment(
+        id = id,
+        name = name,
+        age = age,
+        house_id = house_id,
+        house_zip = house_zip,
+        payment_date = payment_date,
+        payment_date_year = payment_date_year,
+        payment_date_month = payment_date_month,
+        payment_date_day_of_week = payment_date_day_of_week,
+        payment_date_day_of_month = payment_date_day_of_month,
+        payment_amount = payment_amount,
+        rent_amount = rent_amount,
+        default_amount = default_amount
+      )
     }
-    LocalDate.parse(dateStr, DateTimeFormatter.ofPattern(pattern))
-  }
 
-  private def cleanDob(dobStr: String, defaultDobStr: String): LocalDate = {
-    val dob: LocalDate = toLocalDate(dobStr)
-    val default: LocalDate = toLocalDate(defaultDobStr)
-    if (dob.isBefore(today)) {
-      val year: Int = 1900
-      val month: Int = 1
-      val dayOfMonth: Int = 1
-      val invalidDate: LocalDate = LocalDate.of(year, month, dayOfMonth)
-      if (dob.isEqual(invalidDate)) {
-        default
+    private def paymentAmount(opa: Option[Int], ra: Int): Int = opa match {
+      case Some(pa) =>
+        Math.min(Math.abs(pa), ra)
+      case _ => 0
+    }
+
+    private def toDate(localDate: LocalDate): Date = {
+      Date.valueOf(localDate)
+    }
+
+    private def toLocalDate(dateStr: String): LocalDate = {
+      val pattern: String = if (dateStr contains "/") {
+        "MM/dd/yyyy"
       } else {
-        dob
+        "yyyyMMdd"
       }
-    } else {
-      default
+      LocalDate.parse(dateStr, DateTimeFormatter.ofPattern(pattern))
+    }
+
+    private def cleanDob(dobStr: String, defaultDobStr: String): LocalDate = {
+      val dob: LocalDate = toLocalDate(dobStr)
+      val default: LocalDate = toLocalDate(defaultDobStr)
+      if (dob.isBefore(today)) {
+        val year: Int = 1900
+        val month: Int = 1
+        val dayOfMonth: Int = 1
+        val invalidDate: LocalDate = LocalDate.of(year, month, dayOfMonth)
+        if (dob.isEqual(invalidDate)) {
+          default
+        } else {
+          dob
+        }
+      } else {
+        default
+      }
     }
   }
+
 }
+
+
